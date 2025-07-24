16b66f04267c7046fa8c7a61c89743f38649e0ac
Matheus Gouveia <matheusgouveiadev42@gmail.com>
2025-07-24 19:33:36 -0300

    Add files via upload

---
+++ Projeto 1/Mémoria Minada 3.0/desafio_at.js
@@ -0,0 +1,266 @@
+//Quadrado A - Azul
+var a = document.getElementById('a')
+a.addEventListener('click', AzulA)
+function AzulA(){
+    a.innerText = 'AZUL'
+    a.style.background = 'blue'
+    a.style.fontSize = '1rem'
+    a.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado B - Amarelo
+var b = document.getElementById('b')
+b.addEventListener('click', AmareloB)
+function AmareloB(){
+    b.innerText = 'AMARELO'
+    b.style.background = 'yellow'
+    b.style.fontSize = '1rem'
+    b.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado C - Roxo
+var c = document.getElementById('c')
+c.addEventListener('click', RoxoC)
+function RoxoC(){
+    c.innerText = 'ROXO'
+    c.style.background = 'purple'
+    c.style.fontSize = '1rem'
+    c.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado D - Verde 
+var d = document.getElementById('d')
+d.addEventListener('click', VerdeD)
+function VerdeD(){
+    d.innerText = 'VERDE'
+    d.style.background = 'green'
+    d.style.fontSize = '1rem'
+    d.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado E - Marrom
+var e = document.getElementById('e')
+e.addEventListener('click', MarromE)
+function MarromE(){
+    e.innerText = 'MARROM'
+    e.style.background = '#5A270F' //Brown
+    e.style.fontSize = '1rem'
+    e.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado F - Verde
+var f = document.getElementById('f')
+f.addEventListener('click', VerdeF)
+function VerdeF(){
+    f.innerText = 'VERDE'
+    f.style.background = 'green'
+    f.style.fontSize = '1rem'
+    f.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado G - Amarelo
+var g = document.getElementById('g')
+g.addEventListener('click', AmareloG)
+function AmareloG(){
+    g.innerText = 'AMARELO'
+    g.style.background = 'yellow'
+    g.style.fontSize = '1rem'
+    g.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado H - Cinza
+var h = document.getElementById('h')
+h.addEventListener('click', Cinzah)
+function Cinzah(){
+    h.innerText = 'CINZA'
+    h.style.background = '#696969' //Gray
+    h.style.fontSize = '1rem'
+    h.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado I - Branco
+var i = document.getElementById('i')
+i.addEventListener('click', BrancoI)
+function BrancoI(){
+    i.innerText = 'BRANCO'
+    i.style.background = 'white'
+    i.style.fontSize = '1rem'
+    i.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado J - Orange
+var j = document.getElementById('j')
+j.addEventListener('click', Orangej)
+function Orangej(){
+    j.innerText = 'LARANJA'
+    j.style.background = '#ff6600' //Orange
+    j.style.fontSize = '1rem'
+    j.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado K - Bomba!
+var k = document.getElementById('k')
+k.addEventListener('click', BombaK)
+function BombaK(){
+    k.innerText = 'BOMBA!💣'
+    k.style.background = 'linear-gradient(to right, red, yellow)'
+    k.style.fontSize = '1rem'
+    k.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado L - Dourado
+var l = document.getElementById('l')
+l.addEventListener('click', DouradoL)
+function DouradoL(){
+    l.innerText = 'DOURADO'
+    l.style.background = '#d4af37' //Gold
+    l.style.fontSize = '1rem'
+    l.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado M - Aqua
+var m = document.getElementById('m')
+m.addEventListener('click', AquaM)
+function AquaM(){
+    m.innerText = 'AQUA'
+    m.style.background = '#00ffff' //Aqua
+    m.style.fontSize = '1rem'
+    m.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado N - Branco
+var n = document.getElementById('n')
+n.addEventListener('click', BrancoN)
+function BrancoN(){
+    n.innerText = 'BRANCO'
+    n.style.background = 'white'
+    n.style.fontSize = '1rem'
+    n.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado O - Aqua
+var o = document.getElementById('o')
+o.addEventListener('click', AquaO)
+function AquaO(){
+    o.innerText = 'AQUA'
+    o.style.background = '#00ffff' //Aqua
+    o.style.fontSize = '1rem'
+    o.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado P - Azul
+var p = document.getElementById('p')
+p.addEventListener('click', AzulP)
+function AzulP(){
+    p.innerText = 'AZUL'
+    p.style.background = 'blue'
+    p.style.fontSize = '1rem'
+    p.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado Q - Rosa
+var q = document.getElementById('q')
+q.addEventListener('click', RosaQ)
+function RosaQ(){
+    q.innerText = 'ROSA'
+    q.style.background = '#FF007F' //Pink
+    q.style.fontSize = '1rem'
+    q.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado R - Laranja
+var r = document.getElementById('r')
+r.addEventListener('click', LaranjaR)
+function LaranjaR(){
+    r.innerText = 'LARANJA'
+    r.style.background = '#ff6600' //Orange
+    r.style.fontSize = '1rem'
+    r.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado S - Dourado
+var s = document.getElementById('s')
+s.addEventListener('click', DouradoS)
+function DouradoS(){
+    s.innerText = 'DOURADO'
+    s.style.background = '#d4af37' //Gold
+    s.style.fontSize = '1rem'
+    s.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado T - Marrom
+var t = document.getElementById('t')
+t.addEventListener('click', MarromT)
+function MarromT(){
+    t.innerText = 'MARROM'
+    t.style.background = '#5A270F' //Brown
+    t.style.fontSize = '1rem'
+    t.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado U - Roxo
+var u = document.getElementById('u')
+u.addEventListener('click', RoxoU)
+function RoxoU(){
+    u.innerText = 'ROXO'
+    u.style.background = 'purple'
+    u.style.fontSize = '1rem'
+    u.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado V - Bege
+var v = document.getElementById('v')
+v.addEventListener('click', BegeV)
+function BegeV(){
+    v.innerText = 'BEGE'
+    v.style.background = '#e6e6cbff' //Beige
+    v.style.fontSize = '1rem'
+    v.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado V - Bege
+var v = document.getElementById('v')
+v.addEventListener('click', BegeV)
+function BegeV(){
+    v.innerText = 'BEGE'
+    v.style.background = '#e6e6cbff' //Beige
+    v.style.fontSize = '1rem'
+    v.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado W - Bege
+var w = document.getElementById('w')
+w.addEventListener('click', BegeW)
+function BegeW(){
+    w.innerText = 'BEGE'
+    w.style.background = '#e6e6cbff' //Beige
+    w.style.fontSize = '1rem'
+    w.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado X - Rosa
+var x = document.getElementById('x')
+x.addEventListener('click', RosaX)
+function RosaX(){
+    x.innerText = 'ROSA'
+    x.style.background = '#FF007F' //Pink
+    x.style.fontSize = '1rem'
+    x.style.textShadow = '1px 1px 1px black'
+}
+
+//Quadrado Y - Cinza
+var y = document.getElementById('y')
+y.addEventListener('click', CinzaY)
+function CinzaY(){
+    y.innerText = 'CINZA'
+    y.style.background = '#696969' //Gray
+    y.style.fontSize = '1rem'
+    y.style.textShadow = '1px 1px 1px black'
+}
+
+//Botão Reiniciar
+
+function reiniciarJogo(){
+    location.reload()
+}
+document.getElementById('reiniciar').addEventListener('click', reiniciarJogo)
