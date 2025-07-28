ed4406d05c9b5123ef0b5abd8997faf853b4a7e9
Matheus Gouveia <matheusgouveiadev42@gmail.com>
2025-07-28 02:21:21 -0300

    Add files via upload

---
+++ Projeto_Hora_Do_Dia_2.0/HoraDoDia.js
@@ -0,0 +1,36 @@
+function carregar () {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var horas = document.getElementById('horas')
+    var minutos = document.getElementById('minutos')
+    var segundos = document.getElementById('segundos')
+    var data = new Date()
+
+const relogio = setInterval(function time(){
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    var segundo = data.getSeconds()
+
+    if (hora < 10) hora = '0' + hora
+    if (minutos < 10) minutos = '0' + minutos
+    if (segundos < 10) segundos = '0' + segundos
+
+    horas.textContent = hora 
+    minutos.textContent = minuto
+    segundos.textContent = segundo
+})    
+
+    msg.innerHTML = `Agora são:`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'Manhã_Hora_Do_Dia_IMG.jpg'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'Tarde_Hora_Do_Dia_IMG.jpg'
+        document.body.style.background = '#C2BBA7'
+    } else {
+        //BOA NOITE
+        img.src = 'Noite_Hora_Do_Dia_IMG.jpg'
+        document.body.style.background = '#60727D'
+    }
+}
